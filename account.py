fef69d137d9b4c1f0bde4f3ebeef58e1afdf2075
thomas-nguyen320 <nguyenthomas916@gmail.com>
2023-04-16 04:41:37 -0500

    Added docstrings/type hinting for account.py and added test_account.py

--- account.py
+++ account.py
@@ -1,21 +1,49 @@
 class Account:
-    def __init__(self, name):
-        self.__account_name = name
-        self.__account_balance = 0
+    def __init__(self, name,balance = 0) -> None:
+        '''
+        Constructor to create initial state of a Account object.
 
-    def deposit(self,amount):
+        :param name: Person's first name
+        :param balance: Person's account balance
+        '''
+        self.__account_name = name
+        self.__account_balance = balance
+
+    def deposit(self,amount: float) -> bool:
+        '''
+        This function will deposit the amount of money the user enters
+
+        :param amount: This is the amount to deposit
+        :return: Returns a boolean value if successful or not
+        '''
         if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
-    def withdraw(self,amount):
+    def withdraw(self,amount: float) -> bool:
+        '''
+        This function will withdraw the amount of money the user enters
+
+        :param amount: This is the amount to withdraw
+        :return: Returns a boolean value if successful or not
+        '''
         if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
             return False
-    def get_balance(self):
+    def get_balance(self) -> float:
+        '''
+        This function will get display the balance of the user
+
+        :return: Returns the account balance of the user
+        '''
         return self.__account_balance
-    def get_name(self):
+    def get_name(self) -> str:
+        '''
+        This function will display the name of the user
+
+        :return: Returns the account name of the user
+        '''
         return self.__account_name
---
+++ test_account.py
@@ -0,0 +1,63 @@
+from account import *
+
+class Test:
+    def setup_method(self):
+        self.account1 = Account('Jane', 10)
+        self.account2 = Account('John', 20)
+
+    def teardown_method(self):
+        del self.account1
+        del self.account2
+
+    def test_init(self):
+        assert self.account1.get_name() == 'Jane'
+        assert self.account2.get_name() == 'John'
+        assert self.account1.get_balance() == 10
+        assert self.account2.get_balance() == 20
+
+    def test_deposit(self):
+        assert self.account1.deposit(10) is True
+        assert self.account1.get_balance() == 20
+        assert self.account2.deposit(10) is True
+        assert self.account2.get_balance() == 30
+
+        assert self.account1.deposit(10.5) is True
+        assert self.account1.get_balance() == 30.5
+        assert self.account2.deposit(10.5) is True
+        assert self.account2.get_balance() == 40.5
+
+        assert self.account1.deposit(0) is False
+        assert self.account1.get_balance() == 30.5
+        assert self.account2.deposit(0) is False
+        assert self.account2.get_balance() == 40.5
+
+        assert self.account1.deposit(-1) is False
+        assert self.account1.get_balance() == 30.5
+        assert self.account2.deposit(-1) is False
+        assert self.account2.get_balance() == 40.5
+
+
+
+
+    def test_withdraw(self):
+        assert self.account1.withdraw(5) == True
+        assert self.account1.get_balance() == 5
+        assert self.account2.withdraw(5) == True
+        assert self.account2.get_balance() == 15
+
+        assert self.account1.withdraw(2.5) == True
+        assert self.account1.get_balance() == 2.5
+        assert self.account2.withdraw(2.5) == True
+        assert self.account2.get_balance() == 12.5
+
+        assert self.account1.withdraw(0) == False
+        assert self.account1.get_balance() == 2.5
+        assert self.account2.withdraw(0) == False
+        assert self.account2.get_balance() == 12.5
+
+        assert self.account1.withdraw(-1) == False
+        assert self.account1.get_balance() == 2.5
+        assert self.account2.withdraw(-1) == False
+        assert self.account2.get_balance() == 12.5
+
+
