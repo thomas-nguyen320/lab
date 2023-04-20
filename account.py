016da30a41a95653292311ac61d04dd2da054fbd
thomas-nguyen320 <130835064+thomas-nguyen320@users.noreply.github.com>
2023-04-19 22:15:06 -0500

    Steps 1-4 Lab 12

--- account.py
+++ account.py
@@ -1,7 +1,7 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name, balance = 0):
         self.__account_name = name
-        self.__account_balance = 0
+        self.__account_balance = balance
 
     def deposit(self,amount):
         if amount > 0:
